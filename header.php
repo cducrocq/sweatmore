3ec785cbfa8d048f4910634add88082b19cf10e5
Bertrand Defachel <defachel.bertrand@gmail.com>
2019-08-27 13:22:04 +0200

    ajout header + banniere

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+		<meta charset="utf-8">
+		<title>SweatMore</title>
+		<meta name="description" content="Site de sport">
+		<meta name="author" content="SweatMore">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		
+		<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css%22%3E">
+
+	</head>
+<body>
+<header>
+<div class="jumbotron jumbotron-fluid mb-0">
+  <div class="container-fluid">
+    <div class="display-4"><img src="./images/banniere.png" class="img-fluid" alt="banniere de SweatMore"/></div>
+  </div>
+</div>
+</header>
