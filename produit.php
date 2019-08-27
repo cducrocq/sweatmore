6355d8ab3a41fc2a4cbb6f37a63cd463adfd1c7d
Sebastien Colbert <colbertsebastien@gmail.com>
2019-08-27 14:45:49 +0200

    page produit

--- produit.php
+++ produit.php
@@ -1,7 +1,32 @@
-<?php include('header.php'); ?>
+<div class="produit">
+    <div class="photo"><img src="https://via.placeholder.com/900x600/" alt="Photo du produit"></div>
+    <div class="description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas commodi nisi eum molestiae cupiditate officiis ea rem aspernatur sunt ipsam doloremque quidem, unde aliquam, cumque ipsa beatae. Error, ipsum architecto.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, pariatur ullam. Ullam quia alias sapiente eum dolorem explicabo quas eligendi laboriosam ducimus aliquid minus rem, voluptas fuga iste fugiat porro.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure harum consequatur tempore incidunt cum odit sit aliquid soluta modi similique, quibusdam beatae aut animi, commodi dolor amet dicta autem id.</p>
+    </div>
 
+    <form action="index.php">
+        <div class="form-group">
+            <label for="size">Taille</label>
+            <select class="form-control" id="size">
+                <option>XXL</option>
+                <option>3XL</option>
+                <option>4XL</option>
+                <option>5XL</option>
+                <option>6XL</option>
+            </select>
+        </div>
 
-<h1>Produit</h1>
-
-
-<?php include('footer.php'); ?>
+        <div class="form-group">
+            <label for="color">Couleur</label>
+            <select class="form-control" id="color">
+                <option>Rouge</option>
+                <option>Vert</option>
+                <option>Bleu</option>
+                <option>Orange</option>
+                <option>Jaune</option>
+            </select>
+        </div>    
+    </form>
+</div>
