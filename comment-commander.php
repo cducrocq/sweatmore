87e07121f1e6cb26cb1efb4843db019300212dfe
Sebastien Colbert <colbertsebastien@gmail.com>
2019-08-28 16:38:22 +0200

    Ajout de plusieurs pages

---
+++ comment-commander.php
@@ -0,0 +1,21 @@
+<div class="container">
+
+    <h1>Comment Commander ?</h1>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo ea repudiandae, aliquid doloribus, nisi inventore necessitatibus voluptate porro temporibus quas nesciunt quasi repellat! Officiis reiciendis, ipsa quia quidem eius veritatis?</p>
+    <p>Obcaecati quidem recusandae perspiciatis? Eos magni molestiae distinctio nisi dicta, suscipit autem libero quod adipisci sint, nostrum, maxime fuga ut. Aliquam, delectus soluta dolores quasi praesentium dolor ad cum nobis!</p>
+    <p>Dicta aperiam obcaecati sequi modi, voluptatibus officia dolor aut numquam, reprehenderit, assumenda eligendi inventore eaque beatae qui sed est nihil facilis. Assumenda, aliquid. Recusandae itaque dolorem tempore accusamus eius rerum.</p>
+
+    <h2 id="livraison-delais">Livraison & Délais</h2>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo ea repudiandae, aliquid doloribus, nisi inventore necessitatibus voluptate porro temporibus quas nesciunt quasi repellat! Officiis reiciendis, ipsa quia quidem eius veritatis?</p>
+    <p>Obcaecati quidem recusandae perspiciatis? Eos magni molestiae distinctio nisi dicta, suscipit autem libero quod adipisci sint, nostrum, maxime fuga ut. Aliquam, delectus soluta dolores quasi praesentium dolor ad cum nobis!</p>
+    <p>Dicta aperiam obcaecati sequi modi, voluptatibus officia dolor aut numquam, reprehenderit, assumenda eligendi inventore eaque beatae qui sed est nihil facilis. Assumenda, aliquid. Recusandae itaque dolorem tempore accusamus eius rerum.</p>
+
+    <h2 id="droit-retractation">Droit & Rétractation</h2>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo ea repudiandae, aliquid doloribus, nisi inventore necessitatibus voluptate porro temporibus quas nesciunt quasi repellat! Officiis reiciendis, ipsa quia quidem eius veritatis?</p>
+    <p>Obcaecati quidem recusandae perspiciatis? Eos magni molestiae distinctio nisi dicta, suscipit autem libero quod adipisci sint, nostrum, maxime fuga ut. Aliquam, delectus soluta dolores quasi praesentium dolor ad cum nobis!</p>
+    <p>Dicta aperiam obcaecati sequi modi, voluptatibus officia dolor aut numquam, reprehenderit, assumenda eligendi inventore eaque beatae qui sed est nihil facilis. Assumenda, aliquid. Recusandae itaque dolorem tempore accusamus eius rerum.</p>
+
+</div>
