9d3662126ee33fed1e60e7eeffed7b1f569b4df3
Will-H <williamhenard2@gmail.com>
2019-08-28 16:25:06 +0200

    footer version finale !

--- footer.php
+++ footer.php
@@ -1,42 +1,36 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Document</title>
-</head>
-<body>
+
     <footer>
     <div id="hautFooter">
-    <h2 id="servClient">Service client</h2>
-    <i class="fa fa-phone" aria-hidden="true"></i>
-    <p id="numéroTel">+33 (03) 36 30</p>
-    <p id="textFooter">En semaine, vous pouvez nous joindre par téléphone de 9h30 à 18h.</p>
-    <i class="fa fa-envelope" aria-hidden="true"></i>
-    <p id="mailFooter">info@boutiquesweatmore.fr</p>
-    <i class="fa fa-arrow-right" aria-hidden="true"></i>
-    <p id="contactFooter">Contact</p>
-    <br>
+        <h2 id="servClient">Service client</h2>
+        <i class="fa fa-phone" aria-hidden="true"></i>
+        <p id="numéroTel">+33 (03) 36 30</p>
+        <p id="textFooter">En semaine, vous pouvez nous joindre par téléphone de 9h30 à 18h.</p>
+        <i class="fa fa-envelope" aria-hidden="true"></i>
+        <p id="mailFooter">info@boutiquesweatmore.fr</p>
+        <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        <p id="contactFooter">Contact</p>
+        <br>
     </div>
     <div class="basFooter">
         
             <div id=aideInfo>
                 <div id="aideServ">
                     <h2 id="aideService">Aide et service</h2>
-                    <li><a href="index.php?contact"> Contact</a></li>
-                    <li><a href="index.php?index.php?faq"> Foire aux question</a></li>
-                    <li><a href="index.php?comment-commander"> Comment comander</a></li>
-                    <li><a href="index.php?about"> A propos</a></li>
+                    <ul>
+                        <li><a href="index.php?contact"> Contact</a></li>
+                        <li><a href="index.php?index.php?faq"> Foire aux question</a></li>
+                        <li><a href="index.php?comment-commander"> Comment comander</a></li>
+                        <li><a href="index.php?about"> A propos</a></li>
+                    </ul>
                 </div>
                 <div id="info1" >
                     <h2 id="info"><a>Information</a></h2>
-                    <li><a href="index.php?comment-commander"> Livraison et delais</a></li>
-                    <li><a href="index.php?cgv"> CGV</a></li>
-                    <li><a href="index.php?comment-commander#droit-retractation"> Droit de retractation</a></li>
-                    <li><a href="index.php?comment-commander"> Paiement</a></li>
+                    <ul>
+                        <li><a href="index.php?comment-commander"> Livraison et delais</a></li>
+                        <li><a href="index.php?cgv"> CGV</a></li>
+                        <li><a href="index.php?comment-commander#droit-retractation"> Droit de retractation</a></li>
+                        <li><a href="index.php?comment-commander"> Paiement</a></li>
+                    </ul>
                 </div>
                 
             </div>
@@ -82,13 +76,12 @@
         </div>
         
         <div id=copyright>
-        <p>© 2019 Sweetmore GmbH | Boutique sportive, catalogue en ligne d'articles de sport et d'accessoires
-        TVA incluse, port en sus livraison	en sus, taxe d'importation possible hors zone UE.
-        </p>
+            <p>© 2019 Sweetmore GmbH | Boutique sportive, catalogue en ligne d'articles de sport et d'accessoires
+            TVA incluse, port en sus livraison	en sus, taxe d'importation possible hors zone UE.
+            </p>
         </div>
 
         </div>
     </footer>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
-</html>
