c4bceaabce84dd51e3736379871310f8ee777310
Bertrand Defachel <defachel.bertrand@gmail.com>
2019-08-29 15:39:10 +0200

    placement icone Reseau.sociaux

--- social-networks.php
+++ social-networks.php
@@ -1,11 +1,14 @@
-<div id="social-networks">
-    <a href="https://twitter.com" class="btn btn-primary btn-sm" target="_blank" >
-        <i class="fa fa-twitter"></i>
-    </a>
-    <a href="https://instagram" class="btn btn-primary btn-sm" target="_blank" >
-        <i class="fa fa-instagram" aria-hidden="true"></i>
-    </a>
-    <a href="https://facebook" class="btn btn-primary btn-sm" target="_blank" >
-        <i class="fa fa-facebook"></i>
-    </a>
+
+        <div id="social-networks">
+            <a href="https://twitter.com" class="btn btn-primary btn-sm" target="_blank" >
+                <i class="fa fa-twitter"></i>
+            </a>
+            <a href="https://instagram" class="btn btn-primary btn-sm" target="_blank" >
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+            <a href="https://facebook" class="btn btn-primary btn-sm" target="_blank" >
+                <i class="fa fa-facebook"></i>
+            </a>
+        </div>
+    </div>
 </div>
