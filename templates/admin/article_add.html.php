a368b69a6b04af6b68d7d8b1e0f943efa95d9882
Sebastien Colbert <colbertsebastien@gmail.com>
2019-08-30 16:06:22 +0200

    Dev en cours ...

---
+++ templates/admin/article_add.html.php
@@ -0,0 +1,45 @@
+<div class="container mb-5 mt-5">
+
+    <div class="row">
+        <div class="col-sm-12 col-lg-6">
+            <img class="img-fluid rounded float-left" src="https://via.placeholder.com/700/" alt="Photo du produit">
+        </div>
+        <div class="col-sm-12 col-lg-4">
+            <div class="nom">
+                <h2>Mon super article</h2>
+            </div>
+            <div class="description">
+                <p>Quas commodi nisi eum molestiae cupiditate officiis ea rem aspernatur sunt ipsam doloremque quidem, unde aliquam, cumque ipsa beatae. Error, ipsum architecto.</p>
+                <p>Iure, pariatur ullam. Ullam quia alias sapiente eum dolorem explicabo quas eligendi laboriosam ducimus aliquid minus rem, voluptas fuga iste fugiat porro.</p>
+                <p>Iure harum consequatur tempore incidunt cum odit sit aliquid soluta modi similique, quibusdam beatae aut animi, commodi dolor amet dicta autem id.</p>
+            </div>
+
+            <form class="row" action="index.php">
+                <div class="form-group col-sm-12 col-lg-6">
+                    <label for="size">Taille</label>
+                    <select class="form-control" id="size">
+                        <option>XXL</option>
+                        <option>3XL</option>
+                        <option>4XL</option>
+                        <option>5XL</option>
+                        <option>6XL</option>
+                    </select>
+                </div>
+
+                <div class="form-group col-sm-12 col-lg-6">
+                    <label for="color">Couleur</label>
+                    <select class="form-control" id="color">
+                        <option>Rouge</option>
+                        <option>Vert</option>
+                        <option>Bleu</option>
+                        <option>Orange</option>
+                        <option>Jaune</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+
+</div>
--- templates/pages/article.html.php
+++ templates/pages/article.html.php
@@ -1,39 +1,49 @@
-<div class="container mb-5">
+<div class="container mb-5 mt-5">
+
     <div class="row">
-        <div class="col-sm-12 col-lg-4 mt-3 mb-3">
-            <img class="img-fluid rounded float-left" src="https://via.placeholder.com/500/" alt="Photo du produit">
+
+        <div class="col-sm-12 col-lg-6">
+            <img class="img-fluid rounded float-left" src="https://via.placeholder.com/700/" alt="Photo du produit">
         </div>
-        <div class="col-sm-12 col-lg-8 mt-3 mb-3">
+
+        <div class="col-sm-12 col-lg-6">
+            <div class="nom">
+                <h2>Mon super article</h2>
+            </div>
+
+            <hr>
+
             <div class="description">
                 <p>Quas commodi nisi eum molestiae cupiditate officiis ea rem aspernatur sunt ipsam doloremque quidem, unde aliquam, cumque ipsa beatae. Error, ipsum architecto.</p>
                 <p>Iure, pariatur ullam. Ullam quia alias sapiente eum dolorem explicabo quas eligendi laboriosam ducimus aliquid minus rem, voluptas fuga iste fugiat porro.</p>
                 <p>Iure harum consequatur tempore incidunt cum odit sit aliquid soluta modi similique, quibusdam beatae aut animi, commodi dolor amet dicta autem id.</p>
             </div>
+
+            <hr>
+
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-secondary">Tailles</button>
+                <button type="button" class="btn btn-secondary">Left</button>
+                <button type="button" class="btn btn-secondary">Middle</button>
+                <button type="button" class="btn btn-secondary">Right</button>
+            </div>
+            
+            <div class="sizes fles flex">
+                <span>Tailles</span>
+                <div class="circle">XL</div>
+                <div class="circle">XXL</div>
+                <div class="circle">3XL</div>
+            </div>
+           
+            <div class="colors">
+                <span>Couleurs</span>
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+            </div>                      
+
         </div>
+
     </div>
-
-    <form class="row"action="index.php">
-        <div class="form-group col-sm-12 col-lg-6 mt-3 mb-3">
-            <label for="size">Taille</label>
-            <select class="form-control" id="size">
-                <option>XXL</option>
-                <option>3XL</option>
-                <option>4XL</option>
-                <option>5XL</option>
-                <option>6XL</option>
-            </select>
-        </div>
-
-        <div class="form-group col-sm-12 col-lg-6 mt-3 mb-3">
-            <label for="color">Couleur</label>
-            <select class="form-control" id="color">
-                <option>Rouge</option>
-                <option>Vert</option>
-                <option>Bleu</option>
-                <option>Orange</option>
-                <option>Jaune</option>
-            </select>
-        </div>
-    </form>
 
 </div>
