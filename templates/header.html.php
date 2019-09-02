387fc4c14e0c407290739f35cc4267c1b6db29f4
Sebastien Colbert <colbertsebastien@gmail.com>
2019-09-02 07:19:04 +0200

    Ajout du css pour la page article et deplacement du css custom en dernier.

--- templates/header.html.php
+++ templates/header.html.php
@@ -13,11 +13,12 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <!-- CSS Custom -->
-        <link rel="stylesheet" href="css/style.css">
-
         <!-- Font Awesome -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- CSS Custom -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/article.css">
 
     </head>
 
